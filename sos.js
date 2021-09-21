97a62a2fb2ce40bb7c9c2a14cdfb6a08a03edd3e
kofasusi <kacperpocztaa@gmail.com>
2021-09-21 10:09:47 +0200

    zmiany?

--- sos.js
+++ sos.js
@@ -8,12 +8,5 @@ x('hello')
 //comment dsadas
 // git status, git diff, git add, git commit -m "susi"
 
-<<<<<<< HEAD
-//zmiana tak zeby konflikt dzialaj pls ale drugi branch
-//zmiana2354 moze dziala ?????
-//zmiana tak zeby konflikt dzialaj plssasdasadasds
-//zmiana2354saddsa
-=======
 //zmiana tak zeby konflikt dzialaj plss
 //zmiana2354saddsa
->>>>>>> B1
